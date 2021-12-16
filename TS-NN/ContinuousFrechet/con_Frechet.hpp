dbb89d568e107671d73dbbcb935fa82ac3fd8f35
antonis-milios <antonis.milios@gmail.com>
2021-12-16 20:34:43 +0200

    con frechet

--- TS-NN/ContinuousFrechet/con_Frechet.hpp
+++ TS-NN/ContinuousFrechet/con_Frechet.hpp
@@ -8,37 +8,38 @@
 
 namespace cF
 {
-    Curve vecToCurve(Item &item)
-    {
-        Points points(2);
-        for (int i = 0; i < item.xij.size(); i++)
-        {
-            // Coordinates coordinates;
-            // coordinates.push_back((coordinate_t)item.xij[i]);
-            // coordinates.push_back((coordinate_t)i);
-            Point point(2);
-            point.set(0, (coordinate_t)item.xij[i]);
-            point.set(1, (coordinate_t)i);
-            points.add(point);
-        }
+    // Curve vecToCurve(Item &item)
+    // {
+    //     Points points(2);
+    //     for (int i = 0; i < item.xij.size(); i++)
+    //     {
+    //         // Coordinates coordinates;
+    //         // coordinates.push_back((coordinate_t)item.xij[i]);
+    //         // coordinates.push_back((coordinate_t)i);
+    //         Point point(2);
+    //         point.set(0, (coordinate_t)item.xij[i]);
+    //         point.set(1, (coordinate_t)i);
+    //         points.add(point);
+    //     }
 
-        // Curve * curve = new Curve(points, item.id);
-        Curve curve(points, item.id);
-        return curve;
-    }
+    //     // Curve * curve = new Curve(points, item.id);
+    //     Curve curve(points, item.id);
+    //     return curve;
+    // }
 
     Curve vecToCurve(curves::Curve2d &curve)
     {
-        Points points(2);
+        Points points(1);
         for (int i = 0; i < curve.data.size(); i++)
         {
-            // Coordinates coordinates;
-            // coordinates.push_back((coordinate_t)item.xij[i]);
-            // coordinates.push_back((coordinate_t)i);
-            Point point(2);
+            Point point(1);
             point.set(0, (coordinate_t)curve.data[i].x);
-            point.set(1, (coordinate_t)curve.data[i].y);
+
+            Point point2(1);
+            point2.set(0, (coordinate_t)curve.data[i].y);
+
             points.add(point);
+            points.add(point2);
         }
 
         // Curve * fred_curve = new Curve(points, item.id);
@@ -46,10 +47,10 @@ namespace cF
         return fred_curve;
     }
 
-    double distance(Item &item1,  Item &item2)
-    {
-        return Frechet::Continuous::distance(vecToCurve(item1), vecToCurve(item2)).value;
-    }
+    // double distance(Item &item1,  Item &item2)
+    // {
+    //     return Frechet::Continuous::distance(vecToCurve(item1), vecToCurve(item2)).value;
+    // }
     
     double c_distance(curves::Curve2d &item1,  curves::Curve2d &item2)
     {
