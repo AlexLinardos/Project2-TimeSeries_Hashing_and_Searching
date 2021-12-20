13c81d4609503888188a4bf58002a055a82d66c7
AlexLinardos <alexlinardos@gmail.com>
2021-12-20 17:43:45 +0200

    disc Frechet for data

--- TimeSeries-ANN/DiscreteFrechet/disc_Frechet.hpp
+++ TimeSeries-ANN/DiscreteFrechet/disc_Frechet.hpp
@@ -9,7 +9,7 @@
 namespace dF
 {
     // recursive computation of table values for discrete Frechet distance (dynamic programming)
-    double compute_c(double **c, int i, int j, const curves::Curve2d &p, const curves::Curve2d &q)
+    double compute_c(double **c, int i, int j, const std::vector<curves::Point2d> &p, const std::vector<curves::Point2d> &q)
     {
         // if this value has already been computed return it immediately
         if (c[i][j] > -1)
@@ -18,11 +18,11 @@ namespace dF
         }
         else if ((i == 0) && (j > 0))
         {
-            c[0][j] = std::max(compute_c(c, 0, j - 1, p, q), L2(p.data[0].x, p.data[0].y, q.data[j].x, q.data[j].y));
+            c[0][j] = std::max(compute_c(c, 0, j - 1, p, q), L2(p[0].x, p[0].y, q[j].x, q[j].y));
         }
         else if ((i > 0) && (j == 0))
         {
-            c[i][0] = std::max(compute_c(c, i - 1, 0, p, q), L2(p.data[i].x, p.data[i].y, q.data[0].x, q.data[0].y));
+            c[i][0] = std::max(compute_c(c, i - 1, 0, p, q), L2(p[i].x, p[i].y, q[0].x, q[0].y));
         }
         else if ((i > 0) && (j > 0))
         {
@@ -31,7 +31,7 @@ namespace dF
             double c3 = compute_c(c, i, j - 1, p, q);
             double temp_min = std::min(c1, c2);
             double final_min = std::min(temp_min, c3);
-            c[i][j] = std::max(final_min, L2(p.data[i].x, p.data[i].y, q.data[j].x, q.data[j].y));
+            c[i][j] = std::max(final_min, L2(p[i].x, p[i].y, q[j].x, q[j].y));
         }
         return c[i][j];
     }
@@ -69,7 +69,7 @@ namespace dF
         c[0][0] = L2(p.data[0].x, p.data[0].y, q.data[0].x, q.data[0].y);
 
         // compute
-        compute_c(c, pl - 1, ql - 1, p, q);
+        compute_c(c, pl - 1, ql - 1, p.data, q.data);
 
         return c;
     }
@@ -81,30 +81,62 @@ namespace dF
         int ql = q.data.size();
 
         double **c = new double *[pl];
-        try
-        {
-            for (int i = 0; i < pl; i++)
-            {
-                c[i] = new double[ql];
-            }
 
-            // initialize 2d table for dynamic programming with value -1 (since distance cannot be negative)
-            for (int i = 0; i < pl; i++)
-            {
-                for (int j = 0; j < ql; j++)
-                {
-                    c[i][j] = -1;
-                }
-            }
-        }
-        catch (...)
+        for (int i = 0; i < pl; i++)
         {
-            std::cout << "INITIALIZATION" << endl;
-            std::cout << "pl: " << pl << " ql: " << ql << endl;
+            c[i] = new double[ql];
+        }
+
+        // initialize 2d table for dynamic programming with value -1 (since distance cannot be negative)
+        for (int i = 0; i < pl; i++)
+        {
+            for (int j = 0; j < ql; j++)
+            {
+                c[i][j] = -1;
+            }
         }
 
         // base case
         c[0][0] = L2(p.data[0].x, p.data[0].y, q.data[0].x, q.data[0].y);
+
+        // compute
+        compute_c(c, pl - 1, ql - 1, p.data, q.data);
+
+        double result = c[pl - 1][ql - 1];
+
+        for (int i = 0; i < pl; i++)
+        {
+            delete[] c[i];
+        }
+        delete[] c;
+
+        return result;
+    }
+
+    // calculates discrete Frechet distance between only the data attribute of two curves (returns only final value)
+    double discrete_frechet_for_data(const std::vector<curves::Point2d> &p, const std::vector<curves::Point2d> &q)
+    {
+        int pl = p.size();
+        int ql = q.size();
+
+        double **c = new double *[pl];
+
+        for (int i = 0; i < pl; i++)
+        {
+            c[i] = new double[ql];
+        }
+
+        // initialize 2d table for dynamic programming with value -1 (since distance cannot be negative)
+        for (int i = 0; i < pl; i++)
+        {
+            for (int j = 0; j < ql; j++)
+            {
+                c[i][j] = -1;
+            }
+        }
+
+        // base case
+        c[0][0] = L2(p[0].x, p[0].y, q[0].x, q[0].y);
 
         // compute
         compute_c(c, pl - 1, ql - 1, p, q);
